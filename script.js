8fdb34a34c9e821445e820a5fd6cb3b51ca145ba
badiwidya <badiwidya@users.noreply.github.com>
2024-12-05 03:18:27 +0700

    Finish the game and also various fix, explanation:
    Add scores then holding random in a variable, then
    create playGame function that run playRound 5times

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 function getComputerChoice() {
   const choice = ["rock", "paper", "scissor"];
-  return choice[Math.floor(Math.random() * 3 + 1)];
+  const number = Math.floor(Math.random() * 3)
+  return choice[number];
+  
 }
 
 // Get user choice function
@@ -11,8 +13,8 @@ function getUserChoice() {
   return prompt("Rock Paper Scissor: ");
 }
 
-let computerChoice = getComputerChoice();
-let userChoice = getUserChoice();
+let userScore = 0;
+let computerScore = 0;
 
 function playRound(computer, user) {
   const userChoice = String(user).toLocaleLowerCase();
@@ -23,36 +25,58 @@ function playRound(computer, user) {
         break;
       case "paper":
         console.log("You lose! Paper beats Rock.");
+        computerScore += 1;
         break;
       case "scissor":
         console.log("You win! Rock beats Scissor.");
+        userScore += 1;
     }
-  } 
-  else if (userChoice === "paper") {
+  } else if (userChoice === "paper") {
     switch (computer) {
       case "rock":
         console.log("You win! Paper beats Rock");
+        userScore += 1;
         break;
       case "paper":
         console.log("Tie!");
         break;
       case "scissor":
         console.log("You lose! Scissor beats Paper.");
+        computerScore += 1;
     }
-  } 
-  else if (userChoice === "scissor") {
+  } else if (userChoice === "scissor") {
     switch (computer) {
       case "rock":
         console.log("You lose! Rock beats Scissor");
+        computerScore += 1;
         break;
       case "paper":
         console.log("You win! Scissor beats Paper.");
+        userScore += 1;
         break;
       case "scissor":
         console.log("Tie!");
     }
-  } 
-  else {
+  } else {
     console.log("Your choice is only paper, rock, or scissor!");
   }
 }
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    let computerChoice = getComputerChoice();
+    let userChoice = getUserChoice();
+    playRound(computerChoice, userChoice);
+  }
+  if (computerScore > userScore) {
+    console.log(`Computer is the winner! Score: ${computerScore}`);
+    console.log(`You lose! Score: ${userScore}`);
+  } else if (userScore > computerScore) {
+    console.log(`You are the winner! Score: ${computerScore}`);
+    console.log(`Computer lose! Score: ${userScore}`);
+  } else {
+    console.log(`Tie! Score ${userScore}`);
+  }
+}
+
+playGame();
