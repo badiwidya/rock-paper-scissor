6ad67bad258c22adc222477165c48671d22e47c6
badiwidya <badiwidya@users.noreply.github.com>
2024-12-05 19:01:41 +0700

    Rework the script, adding UI to html

--- script.js
+++ script.js
@@ -1,82 +1,54 @@
-// Computer choice function
-
 function getComputerChoice() {
   const choice = ["rock", "paper", "scissor"];
-  const number = Math.floor(Math.random() * 3)
+  const number = Math.floor(Math.random() * 3);
   return choice[number];
-  
 }
 
-// Get user choice function
-
-function getUserChoice() {
-  return prompt("Rock Paper Scissor: ");
+function getUserChoice(e) {
+  return e.target.id;
 }
 
 let userScore = 0;
 let computerScore = 0;
 
-function playRound(computer, user) {
-  const userChoice = String(user).toLocaleLowerCase();
-  if (userChoice === "rock") {
-    switch (computer) {
-      case "rock":
-        console.log("Tie!");
-        break;
-      case "paper":
-        console.log("You lose! Paper beats Rock.");
-        computerScore += 1;
-        break;
-      case "scissor":
-        console.log("You win! Rock beats Scissor.");
-        userScore += 1;
-    }
-  } else if (userChoice === "paper") {
-    switch (computer) {
-      case "rock":
-        console.log("You win! Paper beats Rock");
-        userScore += 1;
-        break;
-      case "paper":
-        console.log("Tie!");
-        break;
-      case "scissor":
-        console.log("You lose! Scissor beats Paper.");
-        computerScore += 1;
-    }
-  } else if (userChoice === "scissor") {
-    switch (computer) {
-      case "rock":
-        console.log("You lose! Rock beats Scissor");
-        computerScore += 1;
-        break;
-      case "paper":
-        console.log("You win! Scissor beats Paper.");
-        userScore += 1;
-        break;
-      case "scissor":
-        console.log("Tie!");
-    }
-  } else {
-    console.log("Your choice is only paper, rock, or scissor!");
+// Select p and span element to display message and score
+const userScoreDisplay = document.querySelector("#score-1>span");
+const computerScoreDisplay = document.querySelector("#score-2>span");
+const msg = document.querySelector("message");
+
+function playGame(e) {
+  let userChoice = getUserChoice(e);
+  let computerChoice = getComputerChoice();
+  switch (userChoice) {
+    case "paper":
+      switch (computerChoice) {
+        case "rock":
+          break;
+        case "scissors":
+          break;
+        case "paper":
+          break;
+      }
+      break;
+    case "rock":
+      switch (computerChoice) {
+        case "rock":
+          break;
+        case "scissors":
+          break;
+        case "paper":
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerChoice) {
+        case "rock":
+          break;
+        case "scissors":
+          break;
+        case "paper":
+          break;
+      }
+      break;
   }
 }
-
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    let computerChoice = getComputerChoice();
-    let userChoice = getUserChoice();
-    playRound(computerChoice, userChoice);
-  }
-  if (computerScore > userScore) {
-    console.log(`Computer is the winner! Score: ${computerScore}`);
-    console.log(`You lose! Score: ${userScore}`);
-  } else if (userScore > computerScore) {
-    console.log(`You are the winner! Score: ${computerScore}`);
-    console.log(`Computer lose! Score: ${userScore}`);
-  } else {
-    console.log(`Tie! Score ${userScore}`);
-  }
-}
-
-playGame();
